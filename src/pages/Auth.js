728340a16e2dcbdb9da424f2b4130d5fccc12d2d
keepforever <briancilenti@gmail.com>
2019-03-12 11:21:56 -0700

    updated login note about slow server magic appropriate favicon

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -17,6 +17,7 @@ export default props => {
 
   return (
     <div style={{ width: '500px', display: "flex", flexDirection: "column" }}>
+
       { isLogin && <Login {...props}/>}
       { !isLogin && <Signup {...props}/>}
       <br/>
@@ -29,6 +30,16 @@ export default props => {
       >
         {isLogin ? "Create An Account" : "Login"}
       </Button>
+      <br />
+      <div style={{color: 'cornsilk', fontSize: 26, lineHeight: 1.5}}>
+        Note:
+        <br />
+        <br />
+        I've noticed the <strong>FREE</strong> endpoint on which I host my server is very slow to respond for the first time each day.  If you are not redirected to the home page after a login attempt, please wait about 1 min, then try again.
+        <br/>
+        <br/>
+        Once the server 'wakes up', it is very responsive and the application should work as normal.
+      </div>
     </div>
   );
 };
